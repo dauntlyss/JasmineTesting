931c813cc3a3f1a6e455dfd59849e3babe47b518
Alyssa Harris <amh302918@gmail.com>
2021-08-16 19:49:15 -0400

    Tested main functions for calculator.js

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,28 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  const values = {
+    amount: 1000,
+    years: 1,
+    rate: 5
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('85.61');
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  const values = {
+    amount: 10006,
+    years: 1,
+    rate: 5
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('856.59');
 });
 
-/// etc
+it("should process very high interest rates", function() {
+  const values = {
+    amount: 10006,
+    years: 1,
+    rate: 99
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('1345.00');
+});
