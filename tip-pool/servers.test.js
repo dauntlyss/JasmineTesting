0bb6d0bfee59a5aec011b7dc69e24abb02b381cd
Alyssa Harris <amh302918@gmail.com>
2021-08-17 10:22:20 -0400

    tested servertable

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -18,7 +18,17 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(Object.keys(allServers).length).toEqual(0);
   });
 
-  
+  it('should update servertable', function () {
+    submitServerInfo();
+    updateServerTable();
+
+    let curTdList = document.querySelectorAll('#serverTable tbody tr td');
+
+    expect(curTdList.length).toEqual(2);
+    expect(curTdList[0].innerText).toEqual('Alice');
+    expect(curTdList[1].innerText).toEqual('$0.00');
+    //expect(curTdList[2].innerText).toEqual('X');
+  });
 
   afterEach(function() {
     serverId = 0;
